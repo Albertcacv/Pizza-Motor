d482eefadf9b60330ada1d483a44e80fc715caa7
albert <55817155+Albertcacv@users.noreply.github.com>
2022-07-22 04:03:00 +0200

    ⚒️ add project structure

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Pizza Motor</title>
+				<meta
+					name='description'
+					content='Pizza Motor is an app for order your favorites pizzas and share them with your friends'
+				/>
+				<link rel='icon' href='/Logo.png' />
+			</Head>
+
+			<h1>HOLA</h1>
+		</div>
+	);
+}
